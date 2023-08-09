614faee0fc3b549a97c12b2a297ccfb0a6d6f76b
Sarmad rafique <sarmadrafique040@gmail.com>
2023-08-09 10:59:15 +0500

    Types props of the app

---
+++ types/Post.ts
@@ -0,0 +1,18 @@
+interface PostProps {
+    id: string,
+    title: string,
+    content: string,
+    liked: boolean,
+    hated: boolean,
+    createdAt: Date,
+    updatedAt: Date,
+    handleLike: (id: string) => void,
+    handleNoLike: (id: string) => void,
+    handleDisLike: (id: string) => void,
+    handleNoHate: (id: string) => void,
+    editPost: (id: string, title: string, content: string) => void,
+    deletePost: (id: string) => void,
+    closeDeleteModal: () => void;
+}
+
+export default PostProps;
