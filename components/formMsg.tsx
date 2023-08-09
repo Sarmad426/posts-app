b48c3374f52a59e07158b5ac0748d8429298ac6c
Sarmad rafique <sarmadrafique040@gmail.com>
2023-08-09 10:58:03 +0500

    Form submit notification

---
+++ components/formMsg.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const FormMsg = () => {
+  return (
+    <div
+      id="msg"
+      className="absolute px-12 py-4 my-4 bottom-1 right-2 rounded-sm bg-green-600 text-gray-300 hidden tracking-wider"
+    >
+      Post Added Successfully 🎊
+    </div>
+  );
+};
+
+export default FormMsg;
